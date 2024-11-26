7d1b8fc118dc46a04cdc0d771f56b70905c86ffb
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-11-27 01:35:25 +0900

    chore: application register in settings

--- src/config/django/base.py
+++ src/config/django/base.py
@@ -21,14 +21,16 @@ THIRD_PARTY_APPS = [
 ]
 
 
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+LOCAL_APPS = [
+    "user.apps.UserConfig",
 ]
+
+INSTALLED_APPS = [
+    *DJANGO_APPS,
+    *THIRD_PARTY_APPS,
+    *LOCAL_APPS,
+]
+
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
@@ -84,6 +86,9 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "user.SocialUser"
+
 LANGUAGE_CODE = "ko-kr"
 
 TIME_ZONE = "Asia/Seoul"
