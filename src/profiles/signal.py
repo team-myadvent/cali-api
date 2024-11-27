6f15591ce861644d8efef76e095b8610b702dc42
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-11-27 22:33:51 +0900

    feat: listen to signal user create

---
+++ src/profiles/signal.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from config.django.base import AUTH_USER_MODEL
+from profiles.models import Profile, Notification
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(post_save, sender=AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwagrs):
+    logger.debug("user profile creating...")
+    if created:
+        notification = Notification.objects.create()
+        Profile.objects.create(user=instance, profile_photo=instance.profile_image, notification=notification)
