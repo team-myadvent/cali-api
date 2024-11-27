bcf40ed4f1f6e9f69e15784a45697ab336da88af
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-11-27 21:41:38 +0900

    feat: social logout

--- src/user/urls.py
+++ src/user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("/kakao", views.KakaoOauthView.as_view(), name="kakao_login"),
     path("/tokens/verify", views.TokenVerifyAPI.as_view(), name="token_verify"),
     path("/tokens/refresh", views.TokenRefreshAPI.as_view(), name="token_refresh"),
+    path("/logout", views.SocialLogutAPI.as_view(), name="logout"),
 ]
