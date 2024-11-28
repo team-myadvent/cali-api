c31d1d8bf2c25bce22c4e29d20930b3b7a6c6c2f
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-11-28 19:03:49 +0900

    fix: variable declare syntax

--- src/config/django/dev.py
+++ src/config/django/dev.py
@@ -6,6 +6,6 @@ CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS = True
 
 INTERNAL_IPS = ["127.0.0.1"]
