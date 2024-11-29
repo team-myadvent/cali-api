33bf98c28941df58c0249a782d66212af1d81ed5
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-11-30 02:24:48 +0900

    chore: allow cors credentials

--- src/config/django/dev.py
+++ src/config/django/dev.py
@@ -2,10 +2,9 @@ from .base import *  # noqa
 
 DEBUG = env.bool("DJANGO_DEBUG", True)
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
-
 ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 INTERNAL_IPS = ["127.0.0.1"]
