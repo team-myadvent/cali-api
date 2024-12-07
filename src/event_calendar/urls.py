488bb31bd394bef00a2840d2bdad5617866f98c7
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-12-08 02:58:36 +0900

    fix: deprecated share urls

--- src/event_calendar/urls.py
+++ src/event_calendar/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 
-from event_calendar.views import views
-
+from event_calendar import views
 
 urlpatterns = [
-    path("", views.EventCalendarListAPI.as_view(), name="calendar-list"),
-    path("/share", views.EventCalendarShareAPI.as_view(), name="share-user-calendar-list"),
-    path("/share/<str:share_key>", views.EventCalendarShareAPI.as_view(), name="share-user-calendar-list"),
+    path("/<int:user_id>", views.EventCalendarListAPI.as_view(), name="calendar-list"),
     path(
-        "/card/<uuid:calendar_id>",
+        "/card/<int:user_id>/<int:calendar_id>",
         views.EventCalendarDetailAPI.as_view(),
         name="calendar-card",
     ),
