c1391e4351802e54521f979ad6db76078b7e3ca3
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-12-18 18:10:08 +0900

    fix: remove the util module and select default data to db

--- src/event_calendar/views/event_calendar_default_page_view.py
+++ src/event_calendar/views/event_calendar_default_page_view.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import AllowAny
 
 from event_calendar.models import EventCalendar
+from event_calendar.serializers.calendar_serializer import CalendarListSerializer, CalendarCardSerializer
 from event_calendar.services.calendar_service import CalendarService
 from shared.api import BaseAPIView
 from utils.calendar.default_calendar import get_default_calendar_data
@@ -12,13 +13,14 @@ class EventCalendarDefaultAPI(BaseAPIView):
 
     def get(self, request):
         query_param = request.query_params.get("idx")
-        default_data = get_default_calendar_data()
 
         if query_param:
-            try:
-                default_data = default_data[int(query_param) - 1]
-                return self.success_response(data=default_data, message="Successfully retrieved calendar")
-            except IndexError:
-                return self.fail_response(message="Does not have calendar idx")
+            calendar_card = self.service.get_event_calendar_by_calendar_id(user=None, calendar_id=query_param)
+            calendar_serializer = CalendarCardSerializer(calendar_card)
+            return self.success_response(
+                data=calendar_serializer.data, message="Successfully retrieved event calendar card"
+            )
 
-        return self.success_response(data=default_data, message="Successfully retrieved calendar")
+        default_calendar = self.service.get_event_calendar_all_for_request_user(user=None)
+        calendar_serializer = CalendarListSerializer(default_calendar)
+        return self.success_response(data=calendar_serializer.data, message="Successfully retrieved calendar")
