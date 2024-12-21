9fb4d584f20ec008ca0310af76649a9d590fb708
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-12-22 00:49:24 +0900

    update: signal to default calendar data by database

--- src/event_calendar/signal.py
+++ src/event_calendar/signal.py
@@ -1,13 +1,10 @@
 import logging
-import random
-from datetime import date
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from config.django.base import AUTH_USER_MODEL
-from config.settings.storages import S3_IMAGE_URL
-from event_calendar.models import EventCalendar, YoutubeMusicEvent
+from event_calendar.models import EventCalendar
 
 logger = logging.getLogger(__name__)
 
@@ -16,25 +13,17 @@ logger = logging.getLogger(__name__)
 def create_user_calendar(sender, instance, created, **kwagrs):
     logger.debug("user default calendar creating...")
 
-    today = date.today()
-    month = 12
-    max_days = 26
-    year = today.year
-
-    seq_values = list(range(1, max_days))
-    random.shuffle(seq_values)
+    queryset = EventCalendar.objects.filter(user=None)
 
     if created:
-        for day, seq in zip(range(1, max_days), seq_values):
-            current_date = date(year, month, day)
-
+        for data in queryset:
             EventCalendar.objects.create(
                 user=instance,
-                youtube=YoutubeMusicEvent.objects.create(user=instance),
-                calendar_dt=current_date,
-                seq_no=day,
-                title=f"{current_date}",
-                comment=f"{current_date}",
-                comment_detail="",
-                default_image=S3_IMAGE_URL.format(seq=seq),
+                youtube=data.youtube,
+                calendar_dt=data.calendar_dt,
+                seq_no=data.seq_no,
+                title=data.title,
+                comment=data.comment,
+                comment_detail=data.comment_detail,
+                default_image=data.default_image,
             )
