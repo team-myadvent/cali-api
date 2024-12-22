9dc30711b77297cd9b2ceaab066ea8ae121ce391
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-12-22 18:23:27 +0900

    fix: production file referring error

--- src/config/django/prd.py
+++ src/config/django/prd.py
@@ -1,8 +1,8 @@
 from .base import *  # noqa
 from .base import env
 
-DEBUG = env.ENV.bool("DJANGO_DEBUG", False)
+DEBUG = env.bool("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = [env.str("ALLOWED_HOSTS", "*")]
+ALLOWED_HOSTS = [env.str("ALLOWED_HOSTS")]
 
-DATABASES = {"default": env.ENV.db("DATABASE_URL")}
+DATABASES = {"default": env.db("DATABASE_URL")}
