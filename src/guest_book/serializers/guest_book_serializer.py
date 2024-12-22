c6b99d7cdb2754b455d2af6495b1fe587eafdfaf
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2024-12-22 20:14:28 +0900

    feat: include users id field in guest book response

--- src/guest_book/serializers/guest_book_serializer.py
+++ src/guest_book/serializers/guest_book_serializer.py
@@ -5,11 +5,13 @@ from guest_book.models import GuestBook
 
 class GuestBookSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source="user.username", read_only=True)
+    userid = serializers.CharField(source="user.social_id", read_only=True)
 
     class Meta:
         model = GuestBook
         fields = [
             "username",
+            "userid",
             "writer_name",
             "content",
         ]
